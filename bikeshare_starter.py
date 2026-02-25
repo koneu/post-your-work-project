5293fb8a81b9eddfa294066b1103e0f1925d3cca
koneu <koneu@example.com>
2026-02-25 12:32:41 +0100

    feat: added newlines

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -2,9 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
+
 
 def get_filters():
     """
@@ -29,6 +31,7 @@ def get_filters():
     return city, month, day
 
 
+
 def load_data(city, month, day):
     """
     Loads data for the specified city and filters by month and day if applicable.
@@ -43,6 +46,7 @@ def load_data(city, month, day):
 
 
     return df
+
 
 
 def time_stats(df):
@@ -64,6 +68,7 @@ def time_stats(df):
     print('-'*40)
 
 
+
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
@@ -83,6 +88,7 @@ def station_stats(df):
     print('-'*40)
 
 
+
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
@@ -97,6 +103,7 @@ def trip_duration_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
+
 
 
 def user_stats(df):
@@ -118,6 +125,7 @@ def user_stats(df):
     print('-'*40)
 
 
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -131,6 +139,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+
 
 
 if __name__ == "__main__":
